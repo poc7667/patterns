2aebf7e8bcc8cd754ee0fe2398aff50d321f11b0
poc7667 <poc7667@gmail.com>
2016-03-01 17:11:59 +0800

    rewrite with active support controller callback function

--- lib/filtering.rb
+++ lib/filtering.rb
@@ -1,29 +1,42 @@
+require 'active_support/concern'
+require 'active_support/callbacks'
+
 module Filtering
-  def self.included(base) # base == ActionController::Base
-    base.extend ClassMethods
+  extend ActiveSupport::Concern
+  include ActiveSupport::Callbacks
+  extend ActiveSupport::Callbacks
+
+  # because ActiveSupport::Concern will do the include/extend tricks for you
+  # def self.included(base) # base == ActionController::Base
+  #   base.extend ClassMethods
+  # end
+
+  included do
+    define_callbacks :process
   end
 
   module ClassMethods
-    def before_action(method)
-      before_actions << method
-    end
 
-    def before_actions
-      @before_actions ||= []
+    def before_action(method)
+      set_callback :process, :before, method
     end
 
     def after_action(method)
-      after_actions << method
+      set_callback :process, :after, method
     end
 
-    def after_actions
-      @after_actions ||= []
-    end
+    # def after_actions
+    #   @after_actions ||= []
+    # end
   end
 
   def process(action_name)
-    self.class.before_actions.each { |method| send method }
-    super
-    self.class.after_actions.each {|method| send method }
+    run_callbacks :process do
+      super
+    end
+
+    # self.class.before_actions.each { |method| send method }
+    # super
+    # self.class.after_actions.each {|method| send method }
   end
 end
